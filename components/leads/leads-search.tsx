396fd4335f86b87a9ba6c1c1800d4173288862f3
Marcos Longo <marcosv.longo@gmail.com>
2026-02-18 10:13:25 -0300

    feat

--- components/leads/leads-search.tsx
+++ components/leads/leads-search.tsx
@@ -37,7 +37,7 @@ export function LeadsSearch() {
   }
 
   return (
-    <form onSubmit={handleSearch} className="relative flex items-center gap-2">
+    <form onSubmit={handleSearch} className="w-1/3 relative flex items-center gap-2">
       <div className="relative flex-1">
         <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
         <Input
